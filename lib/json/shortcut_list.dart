6246617dff48052ae40b3a5fcad614d76ffc5259
Martino Yovo <martinosoyen@gmail.com>
2021-09-13 19:29:26 +0200

    Dummy data

---
+++ lib/json/shortcut_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_banking_app/utils/iconly/iconly_bold.dart';
+
+List shortcutList = [
+  {
+    "color": Color(0xFF026EF4),
+    "icon": IconlyBold.Download
+  },
+  {
+    "color": Color(0xFFFB6A4B),
+    "icon": IconlyBold.Upload
+  },
+  {
+    "color": Color(0xFF2BB33A),
+    "icon": IconlyBold.Wallet
+  },
+  {
+    "color": Color(0xFFAF52C1),
+    "icon": IconlyBold.Category
+  },
+];
---
+++ lib/json/transactions.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_banking_app/generated/assets.dart';
+
+List<Map<String, dynamic>> transactions = [
+  {
+    "icon": CupertinoIcons.house_fill,
+    "name": "Collage Free",
+    "date": "4:56 PM",
+    "amount": "-140"
+  },
+  {
+    "avatar": Assets.memoji1,
+    "name": "Alec Koder",
+    "date": "5:20 PM",
+    "amount": "+100"
+  },
+  {
+    "avatar": Assets.memoji2,
+    "name": "Tino Well",
+    "date": "7:21 PM",
+    "amount": "+110"
+  },
+];
