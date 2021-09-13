189875fccb2d426715f418d01dd74faca618b921
Martino Yovo <martinosoyen@gmail.com>
2021-09-13 19:28:08 +0200

    Buttons

---
+++ lib/widgets/buttons.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_banking_app/utils/size_config.dart';
+import 'package:flutter_banking_app/utils/styles.dart';
+
+Widget elevatedButton({required BuildContext context, required VoidCallback callback, required String text, Color? color}) {
+  return Container(
+    width: double.infinity,
+      //padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+      child: ElevatedButton(
+        child: Text(text),
+        style: ElevatedButton.styleFrom(
+            primary: color == null ? Styles.primaryColor: color,
+            elevation: 0,
+            //shadowColor: Colors.indigoAccent,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(13)),
+            padding: EdgeInsets.symmetric(vertical: 14),
+            textStyle: TextStyle(fontFamily: "DMSans", color: Colors.white, fontWeight: FontWeight.w500, fontSize: 17)),
+        onPressed: callback
+      ),
+  );
+}
+
+Widget outlinedButton({required BuildContext context, required VoidCallback callback, required Widget child, String? color}) {
+  return OutlinedButton(
+    onPressed: callback,
+    child: child,
+    style: OutlinedButton.styleFrom(
+      backgroundColor: Colors.transparent,
+      primary: Styles.primaryColor,
+      elevation: 0,
+      side: BorderSide(color: Colors.grey.shade400, width: 1),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(getProportionateScreenWidth(10))),
+      padding: EdgeInsets.symmetric(vertical: 13, horizontal: 15),
+    ),
+  );
+}
